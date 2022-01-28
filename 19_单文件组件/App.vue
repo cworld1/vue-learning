135684e3247f2e5ee0793afcb70cf4170e54e753
cworld1 <cworld0@qq.com>
2022-01-28 17:26:15 +0800

    Update demo SINGLE-COMPONENTS

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import School from "./School.vue";
+import Student from "./Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
