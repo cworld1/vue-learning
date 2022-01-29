cc209a41ecc70d6ae46ffe474956dccddbf3fc4b
cworld1 <cworld0@qq.com>
2022-01-29 21:52:50 +0800

    Update demo ANALYSE-SCAFFOLD

---
+++ 01_src_分析脚手架/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo" />
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
