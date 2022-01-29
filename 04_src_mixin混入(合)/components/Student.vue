91a13c533b72731ecd5ef68de56bb3d2a935faed
cworld1 <cworld0@qq.com>
2022-01-30 01:37:25 +0800

    Update demo MIXIN

---
+++ 04_src_mixin混入(合)/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 @click="showName">学生姓名: {{ name }}</h2>
+    <h2>学生年龄: {{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+// import { m, m2 } from "../mixin"; // 局部引入
+
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      sex: "男",
+      x: 666,
+    };
+  },
+  // mixins: [m, m2],
+};
+</script>
---
+++ 04_src_mixin混入(合)/main.js
@@ -0,0 +1,16 @@
+// 引入Vue
+import Vue from 'vue'
+// 引入App
+import App from './App.vue'
+// 关闭Vue的生产提示
+Vue.config.productionTip = false
+
+import { m, m2 } from './mixin'
+Vue.mixin(m)
+Vue.mixin(m2)
+
+// 创建vm
+new Vue({
+    el: '#app',
+    render: h => h(App),
+})
