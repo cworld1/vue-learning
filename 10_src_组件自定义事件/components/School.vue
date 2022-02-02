defb54c48f6e33d546c0ad4f8dccf3d35248dd24
cworld1 <cworld0@qq.com>
2022-02-02 11:44:51 +0800

    Update demo CUSTOM-EVENT

---
+++ 10_src_组件自定义事件/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="school">
+    <h2>学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+    <button @click="sendSchoolName">把学校名传给App (Props传方法)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  props: ["getSchoolName"],
+  data() {
+    return {
+      name: "西南大学",
+      address: "重庆市北碚区",
+    };
+  },
+  methods: {
+    sendSchoolName() {
+      this.getSchoolName(this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.school {
+  background-color: skyblue;
+  padding: 10px 15px;
+}
+</style>
