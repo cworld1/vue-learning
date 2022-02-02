9bc5535b6e7d6f00ad9ff1c62b91c88f6d74d130
cworld1 <cworld0@qq.com>
2022-02-02 18:44:27 +0800

    Update demo $BUS

---
+++ 12_src_全局事件总线/components/Student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="student">
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生年龄: {{ sex }}</h2>
+    <button @click="sendStudentName">把学生名给School组件</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      sex: "男",
+    };
+  },
+  methods: {
+    sendStudentName() {
+      this.$bus.$emit("clickToRun", this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.student {
+  background-color: pink;
+  padding: 10px 15px;
+}
+</style>
---
+++ src/components/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="school">
+    <h2>学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "西南大学",
+      address: "重庆市北碚区",
+    };
+  },
+  mounted() {
+    this.$bus.$on("clickToRun", (data) => {
+      console.log("我是School组件，收到了数据:", data);
+    });
+  },
+  beforeDestroy() {
+    this.$bus.$off("clickToRun");
+  },
+};
+</script>
+
+<style scoped>
+.school {
+  background-color: skyblue;
+  padding: 10px 15px;
+}
+</style>
