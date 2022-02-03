b07b1c56480a169cc77f8889e82c805d4670cb6c
cworld1 <cworld0@qq.com>
2022-02-03 22:44:46 +0800

    Update demo TODO-LIST

---
+++ 18_src_TodoList_动画/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="todo-main">
+    <TodoItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+  </ul>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "TodoList",
+  components: { TodoItem },
+  // 声明接收App传递过来的数据
+  props: ["todos"],
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #f1f1f1;
+  border-radius: 4px;
+  padding: 0px;
+  background-color: #fdfdfd;
+  overflow: hidden;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #f1f1f1;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
