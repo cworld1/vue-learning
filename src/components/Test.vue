50ec60b0c04e2cb6b319ce73636164676352949a
cworld1 <cworld0@qq.com>
2022-02-03 19:39:37 +0800

    Update demo TRANSITION&ANIMATE

---
+++ src/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- <transition name="title" :appear="true"> -->
+    <transition name="title" appear>
+      <h1 v-show="isShow">你好啊</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+.title-enter-active {
+  animation: moveToShow 0.5s ease-in-out;
+}
+
+.title-leave-active {
+  animation: moveToShow 0.5s ease-in-out reverse;
+}
+
+@keyframes moveToShow {
+  from {
+    opacity: 0.2;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+</style>
---
+++ src/components/Test2.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- <transition name="title" :appear="true"> -->
+    <transition-group name="title" appear>
+      <h1 v-show="!isShow" key="1">你好啊</h1>
+      <h1 v-show="isShow" key="2">Vuuue!</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+.title-enter-active,
+.title-leave-active {
+  transition: 0.5s ease-in-out;
+}
+
+/* 进入的起点，离开的终点 */
+.title-enter,
+.title-leave-to {
+  transform: translateX(-100%);
+  opacity: 0.2;
+}
+
+/* 进入的终点，离开的起点 */
+.title-enter-to,
+.title-leave {
+  transform: translateX(0);
+  opacity: 1;
+}
+</style>
