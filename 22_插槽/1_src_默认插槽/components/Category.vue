891f89c5bb0fabb82e84ffa08107024d1d1ee034
cworld1 <cworld0@qq.com>
2022-02-05 00:17:21 +0800

    Update demo SLOT

---
+++ 22_插槽/1_src_默认插槽/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <ul>
+      <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+    </ul>
+    <!-- 定义一个插槽（挖个坑，等着组件的使用者进行填充） -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title", "listData"],
+};
+</script>
+
+<style>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+.media {
+  width: 100%;
+  opacity: 0.3;
+}
+</style>
