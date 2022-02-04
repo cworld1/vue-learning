a2cf0b0c0569ba5cd9e6401adb0923fc905f5e9e
cworld1 <cworld0@qq.com>
2022-02-04 21:45:45 +0800

    Update demo GITHUB-SEARCH

---
+++ 21_src_github搜索案例_vue-resource/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <Search />
+    <List />
+  </div>
+</template>
+
+<script>
+import "./assets/css/bootstrap.css";
+
+import Search from "./components/Search.vue";
+import List from "./components/List.vue";
+export default {
+  name: "App",
+  components: { Search, List },
+};
+</script>
+
+<style>
+</style>
