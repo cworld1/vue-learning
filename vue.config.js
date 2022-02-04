91176a5b02f1299ab57671305b1e29d4677b594d
cworld1 <cworld0@qq.com>
2022-02-04 19:05:57 +0800

    Update file vue.config.jd

--- vue.config.js
+++ vue.config.js
@@ -5,27 +5,32 @@
  */
 
 module.exports = {
-    /*     pages: {
-            index: {
-                // page 的入口
-                entry: 'src/index/main.js',
-                // 模板来源
-                template: 'public/index.html',
-                // 在 dist/index.html 的输出
-                filename: 'index.html',
-                // 当使用 title 选项时，
-                // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-                title: 'Index Page',
-                // 在这个页面中包含的块，默认情况下会包含
-                // 提取出来的通用 chunk 和 vendor chunk。
-                chunks: ['chunk-vendors', 'chunk-common', 'index']
+    pages: {
+        index: {
+            entry: 'src/main.js', // page 的入口
+        }
+    },
+
+    lintOnSave: false, // 关闭语法检查
+
+    devServer: {
+        // 开启代理服务器（方式1）
+        // proxy: 'http://localhost:5000' // 开启代理服务器
+
+        // 开启代理服务器（方式2）
+        /* proxy: {
+            '/studentApi': {
+                target: 'http://localhost:5000',
+                pathRewrite: { "^/studentApi": "" },
+                // ws: true, // 用于支持websocket
+                changeOrigin: true // 用于控制请求头中的host值，是否需要与请求服务器相同
             },
-            // 当使用只有入口的字符串格式时，
-            // 模板会被推导为 `public/subpage.html`
-            // 并且如果找不到的话，就回退到 `public/index.html`。
-            // 输出文件名会被推导为 `subpage.html`。
-            subpage: 'src/subpage/main.js'
+            '/carApi': {
+                target: 'http://localhost:5001',
+                pathRewrite: { "^/carApi": "" },
+                // ws: true, // 用于支持websocket
+                changeOrigin: true // 用于控制请求头中的host值，是否需要与请求服务器相同
+            }
         }, */
-    // 关闭语法检查
-    lintOnSave: false
+    }
 }
