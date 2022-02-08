e7e3b742a895cd4ddbd2edc29110e8a802287326
cworld1 <cworld0@qq.com>
2022-02-09 00:09:24 +0800

    Update demo ROUTER

---
+++ 33_src_命名路由/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-header">
+    <h2>Vue Router Demo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style>
+</style>
---
+++ 33_src_命名路由/pages/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <hr />
+    <h3>{{ $route.query.title }}</h3>
+    <p>资讯编号: {{ $route.query.id }}</p>
+    <p>
+      <img class="img-responsive img-rounded" :src="$route.query.media" />
+    </p>
+    <p>{{ $route.query.summary }}</p>
+    <a
+      :href="`https://ys.mihoyo.com/main/news/detail/${$route.query.id}`"
+      target="_blank"
+      >阅读原文</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+};
+</script>
+
+<style>
+</style>
