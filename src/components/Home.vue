13594ae53cd07ecdd5cc72ea2a83b1a14c282799
cworld1 <cworld0@qq.com>
2022-02-08 20:15:09 +0800

    Update demo ROUTER

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div panel-body>
+    <h2>我是Home的内容</h2>
+    <img
+      src="https://webstatic.mihoyo.com/upload/static-resource/2022/02/07/c94065dbae7417928f7c28e20080d13d_8373427145217674191.jpg"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+</style>
