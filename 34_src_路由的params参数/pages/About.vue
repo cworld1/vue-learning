523b1fc8e3d6fbc20c0177a6fa15d0055dca85f9
cworld1 <cworld0@qq.com>
2022-02-09 00:25:44 +0800

    Update demo ROUTER

---
+++ 34_src_路由的params参数/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Home组件内容</h2>
+
+    <ul>
+      <li>我学习的是Vue</li>
+      <li>我在B站学习学习</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style>
+</style>
