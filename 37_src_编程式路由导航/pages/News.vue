af8838f0a1fcbde37dc1a88edb03581f5232acd1
cworld1 <cworld0@qq.com>
2022-02-09 13:44:34 +0800

    Update demo ROUTER

---
+++ 37_src_编程式路由导航/pages/News.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div v-for="i in newsList" :key="i.id" class="media">
+      <div class="media-body">
+        <h4 class="media-heading">
+          <!-- 跳转路由并携带query参数/params路径，to的字符串写法 -->
+          <!-- <router-link
+            :to="`/home/news/detail/${i.id}?title=${i.title}&media=${i.media}&summary=${i.summary}`"
+            >{{ i.title }}</router-link
+          > -->
+
+          <!-- 跳转路由并携带query参数/params路径，to的字符串写法 -->
+          <router-link
+            :to="{
+              name: 'detail',
+              params: {
+                id: i.id,
+              },
+              query: {
+                title: i.title,
+                media: i.media,
+                summary: i.summary,
+              },
+            }"
+            >{{ i.title }}</router-link
+          >
+        </h4>
+        {{ i.summary }}
+      </div>
+      <div class="media-right">
+        <img
+          class="media-object img-rounded"
+          :src="i.media"
+          style="width: 152px; height: 72px"
+        />
+      </div>
+    </div>
+    <router-view></router-view>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+  data() {
+    return {
+      newsList: [
+        {
+          id: "19842",
+          title: "原神｜这璃月港的烟火，是本仙第二喜欢的光景了。",
+          summary:
+            "这璃月港的烟火，是本仙第二喜欢的光景了。还记得你小时候——嗯？这里又没有别人，听我讲完。",
+          media:
+            "https://webstatic.mihoyo.com/upload/static-resource/2022/01/28/11728e9b7d76362a227ea8d8f7fae510_481008032604213174.png",
+        },
+        {
+          id: "19841",
+          title: "原神｜枪缨猎猎平诸事…",
+          summary: "枪缨猎猎平诸事，烟火灿灿祈平安。",
+          media:
+            "https://webstatic.mihoyo.com/upload/static-resource/2022/01/28/14277320177adb9b306eeb7626887406_3412169448056666151.png",
+        },
+        {
+          id: "19840",
+          title: "原神｜这次出海兄弟们都干劲十足，果然收获颇丰啊！",
+          summary:
+            "哈哈，这次出海兄弟们都干劲十足，果然收获颇丰啊！谁不想过节多带点礼物回家呢，对吧？",
+          media:
+            "https://webstatic.mihoyo.com/upload/static-resource/2022/01/28/278266764db479aa4c2ece94b9e04c2f_9182845150069458133.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 37_src_编程式路由导航/router/index.js
@@ -0,0 +1,59 @@
+// 该文件用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+// 引入组件
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+import Content from '../pages/Content.vue'
+import News from '../pages/News.vue'
+import Detail from '../pages/Detail.vue'
+
+// 创建一个路由器
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      children: [
+        {
+          path: 'content',
+          component: Content
+
+        },
+        {
+          path: 'news',
+          component: News,
+          children: [
+            {
+              name: 'detail',
+              path: 'detail/:id',
+              component: Detail,
+              // props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件。
+              // props: {a:1,b:'hello'}
+
+              // props的第二种写法，值为布尔值，若布尔值为真，就会把该路由组件收到的所有params参数，以props的形式传给Detail组件。
+              // props: true
+
+              // props的第二种写法，值为函数
+              /* props($router) {
+                return {
+                  id: $router.params.id,
+                  title: $router.query.title,
+                  media: $router.query.media,
+                  summary: $router.query.summary,
+                } */
+              props({ params: { id }, query: { title, media, summary } }) {
+                return { id, title, media, summary }
+              }
+            }
+          ]
+
+        },
+      ]
+    },
+    {
+      name: 'about',
+      path: '/about',
+      component: About
+    },
+  ]
+})
