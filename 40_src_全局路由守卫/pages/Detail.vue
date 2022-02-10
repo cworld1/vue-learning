9d4380d09a2b9f8aba6ca339c61a446425a64cf3
cworld1 <cworld0@qq.com>
2022-02-10 23:54:19 +0800

    Update demo ROUTER

---
+++ 40_src_全局路由守卫/pages/Detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <hr />
+    <h3>{{ title }}</h3>
+    <p>资讯编号: {{ id }}</p>
+    <p>
+      <img class="img-responsive img-rounded" :src="media" />
+    </p>
+    <p>{{ summary }}</p>
+    <a :href="`https://ys.mihoyo.com/main/news/detail/${id}`" target="_blank"
+      >阅读原文</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: ["id", "title", "media", "summary"],
+};
+</script>
+
+<style>
+</style>
---
+++ 40_src_全局路由守卫/pages/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h2>登录</h2>
+    <div class="alert alert-danger" role="alert">
+      <span class="glyphicon glyphicon-exclamation-sign"></span>
+      请先登录后查看详情
+    </div>
+
+    <div class="well col-xs-offset-2 col-xs-8">
+      <h4>登录</h4>
+      <div class="input-group">
+        <span class="input-group-addon">
+          <span class="glyphicon glyphicon-envelope"></span>
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Mail"
+          v-model="mail"
+        />
+      </div>
+
+      <div class="input-group">
+        <span class="input-group-addon">
+          <span class="glyphicon glyphicon-sunglasses"></span>
+        </span>
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="psw"
+        />
+      </div>
+      <button class="btn btn-primary" @click="saveInfo">登录</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      mail: "",
+      psw: "",
+    };
+  },
+  methods: {
+    saveInfo() {
+      localStorage.setItem("mail", this.mail);
+      localStorage.setItem("psw", this.psw);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-group {
+  margin-bottom: 18px;
+}
+</style>
