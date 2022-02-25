cf0799478eda73da72f8b4dd5578747e296ceea3
cworld1 <cworld0@qq.com>
2022-02-25 19:47:24 +0800

    fix: fix a bug for wrong typing

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-modules.exports = {
-    // 关闭 ESLint
-    lintOnSave: flase
-}
+module.exports = {
+    // productionSourceMap: false,
+    // 关闭ESLINT校验工具
+    lintOnSave: false,
+    //配置代理跨域
+    /* devServer: {
+      proxy: {
+        "/api": {
+          target: "http://39.98.123.211",
+        },
+      },
+    }, */
+};
