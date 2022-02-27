4f98d215898916a95099cfa846fe3f771f19fcd5
cworld1 <cworld0@qq.com>
2022-02-27 19:00:29 +0800

    refactor(main.js): edit main.js to register the global component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+// 三级联动组件（商品分类导航）
+import TypeNav from '@/pages/Home/TypeNav'
+
+// 注册全局组件（参数一：全局组件名称；参数二：哪一个组件）
+Vue.component(TypeNav.name, TypeNav)
 
 Vue.config.productionTip = false
 
