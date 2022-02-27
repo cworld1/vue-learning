385240b11233230aabb866a9cce542549bcfaf02
cworld1 <cworld0@qq.com>
2022-02-27 18:57:53 +0800

    refactor(component): edit home page to make the components effictive

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>主页</div>
+  <div>
+    <!-- 商品分类导航 -->
+    <TypeNav />
+    <!--列表-->
+    <ListContainer />
+    <!--今日推荐-->
+    <Recommend />
+    <!-- 商品排行 -->
+    <Rank />
+    <!-- 猜你喜欢 -->
+    <Like />
+    <!--楼层-->
+    <Floor />
+    <!--楼层-->
+    <Floor />
+    <!--商标-->
+    <Brand />
+  </div>
 </template>
 
 <script>
+import ListContainer from "./ListContainer";
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
 export default {
   name: "Home",
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
 };
 </script>
 
