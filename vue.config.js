ab8b9ae140db0a1c2964f33f9f27263b7fe43d37
cworld1 <cworld0@qq.com>
2022-02-28 13:55:23 +0800

    ci(vue.config.js): edit the proxy server config to make effictive

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    // productionSourceMap: false,
-    // 关闭ESLINT校验工具
-    lintOnSave: false,
-    //配置代理跨域
-    /* devServer: {
-      proxy: {
-        "/api": {
-          target: "http://39.98.123.211",
-        },
+  // productionSourceMap: false,
+  // 关闭 ESLint 校验工具
+  lintOnSave: false,
+  //配置代理跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://39.98.123.211",
       },
-    }, */
+    },
+  },
 };
