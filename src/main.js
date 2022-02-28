11f9c2802b4001a0611883cdb5c10fd237823aad
cworld1 <cworld0@qq.com>
2022-02-28 19:08:37 +0800

    fix(main.js): fix main.js to make efficts effort

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from '@/router'
-// 三级联动组件（商品分类导航）
-import TypeNav from '@/pages/Home/TypeNav'
+import router from './router'
+
+// 引入由 Vuex 构建的 store 状态管理模块
+import store from './store'
+
+// 引入商品分类导航组件（商品分类导航）
+import TypeNav from './components/TypeNav'
 
 // 注册全局组件（参数一：全局组件名称；参数二：哪一个组件）
 Vue.component(TypeNav.name, TypeNav)
@@ -11,5 +15,8 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 使用路由组件
   router,
+  // 使用 vuex 组件
+  store
 }).$mount('#app')
