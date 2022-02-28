192360572173f9ba0fb85aa84b8fdc9f4594979c
cworld1 <cworld0@qq.com>
2022-02-28 19:07:36 +0800

    feat(api): update api to make useful

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+// 对当前 api 进行统一管理
+import requests from "./request";
+
+// 商品分类导航
+// /api/product/getBaseCategoryList get 无参数
+export const reqTypeNav = () => requests({ url: 'product/getBaseCategoryList', method: 'get' })
