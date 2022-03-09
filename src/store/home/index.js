385f5447b220201199b9adcfb007eaf6d5753fdd
cworld1 <cworld0@qq.com>
2022-03-10 00:08:26 +0800

    feat(store): add banner data from api

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,22 +1,37 @@
 // home 模块下的 store 仓库
 
 // 引入商品分类导航信息获取 api
-import { reqTypeNav } from "@/api";
+import { reqGetTypeNavList, reqGetBannerList } from "@/api";
 
 const state = {
+  // 商品分类导航
   typeNavData: [],
+  // 首页广告轮播
+  bannerData: [],
 };
 const mutations = {
-  ADDTYPENAV(state, data) {
+  // 商品分类导航
+  ADDTYPENAVLIST(state, data) {
     state.typeNavData = data;
+  },
+  // 首页广告轮播
+  ADDBANNERLIST(state, data) {
+    state.bannerData = data;
   },
 };
 const actions = {
-  // 向服务器发请求
-  async getTypeNav({ commit }) {
-    let result = await reqTypeNav();
+  // 商品分类导航
+  async getTypeNavList({ commit }) {
+    let result = await reqGetTypeNavList();
     if (result.code == 200) {
-      commit("ADDTYPENAV", result.data);
+      commit("ADDTYPENAVLIST", result.data);
+    }
+  },
+  // 首页广告轮播
+  async getBannerList({ commit }) {
+    let result = await reqGetBannerList();
+    if (result.code == 200) {
+      commit("ADDBANNERLIST", result.data);
     }
   },
 };
