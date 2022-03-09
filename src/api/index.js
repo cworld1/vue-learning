4450916b2259f79101e6a2564767e66a89db3c8e
cworld1 <cworld0@qq.com>
2022-03-10 00:09:51 +0800

    feat(api): add new api reqGetBannerList

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 // 对当前 api 进行统一管理
-import requests from "./request";
+import requests from "./axios";
+import mockRequests from "./mockAjax";
 
 // 商品分类导航
 // /api/product/getBaseCategoryList get 无参数
-export const reqTypeNav = () =>
-  requests({
-    url: "product/getBaseCategoryList",
-    method: "get",
-  });
+export const reqGetTypeNavList = () =>
+  requests.get("/product/getBaseCategoryList");
+
+// 首页广告轮播
+// /mock/product/getBaseCategoryList get 无参数
+export const reqGetBannerList = () => mockRequests.get("/banner");
