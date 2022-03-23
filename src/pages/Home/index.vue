48ee6a24b4e1a7d17d8433014608f335d261f4ec
cworld1 <cworld0@qq.com>
2022-03-23 23:45:38 +0800

    feat(component): add floordata; v-for&props to give data to floor

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -11,9 +11,11 @@
     <!-- 猜你喜欢 -->
     <Like />
     <!--楼层-->
-    <Floor />
-    <!--楼层-->
-    <Floor />
+    <Floor
+      v-for="floor in floorData"
+      :key="floor.id"
+      :singleFloorData="floor"
+    />
     <!--商标-->
     <Brand />
   </div>
@@ -26,14 +28,25 @@ import Rank from "./Rank";
 import Like from "./Like";
 import Floor from "./Floor";
 import Brand from "./Brand";
+
+import { mapState } from "vuex";
 export default {
   name: "Home",
   components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
+  computed: {
+    ...mapState({
+      floorData: (state) => state.home.floorData,
+    }),
+  },
+  mounted() {
+    // 派发 action，获取 Floor 组件的数据
+    this.$store.dispatch("getFloorList");
+  },
 };
 </script>
 
 <style scoped>
 .list-container {
-  margin-bottom: 60px;
+  margin-bottom: 115px;
 }
 </style>
