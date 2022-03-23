938fdd3d2523a54c86ac648ca03b4e3ac4f90623
cworld1 <cworld0@qq.com>
2022-03-23 23:44:15 +0800

    feat(store/vuex): add store data and mutations&aactions floordata

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,13 +1,15 @@
 // home 模块下的 store 仓库
 
 // 引入商品分类导航信息获取 api
-import { reqGetTypeNavList, reqGetBannerList } from "@/api";
+import { reqGetTypeNavList, reqGetBannerList, reqGetFloorList } from "@/api";
 
 const state = {
   // 商品分类导航
   typeNavData: [],
   // 首页广告轮播
   bannerData: [],
+  // 首页 Floor 组件
+  floorData: [],
 };
 const mutations = {
   // 商品分类导航
@@ -17,6 +19,10 @@ const mutations = {
   // 首页广告轮播
   ADDBANNERLIST(state, data) {
     state.bannerData = data;
+  },
+  // 首页 Floor 组件
+  ADDFLOORLIST(state, data) {
+    state.floorData = data;
   },
 };
 const actions = {
@@ -34,8 +40,15 @@ const actions = {
       commit("ADDBANNERLIST", result.data);
     }
   },
+  // 首页 Floor 组件
+  async getFloorList({ commit }) {
+    let result = await reqGetFloorList();
+    if (result.code == 200) {
+      commit("ADDFLOORLIST", result.data);
+    }
+  },
 };
-const getters = {};
+const getters = {}; // 计算属性
 
 export default {
   state,
