a77af7b078483a23292440c50c241f7a753e2cf4
cworld1 <cworld0@qq.com>
2022-03-25 19:03:40 +0800

    feat(component): add new component carousel gloabally

---
+++ src/components/Carousel/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="swiper-container" ref="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="carousel in list" :key="carousel.id">
+        <img :src="carousel.imgUrl" />
+      </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+export default {
+  name: "Carousel",
+  props: ["list"],
+  watch: {
+    list: {
+      immediate: true,
+      handler() {
+        this.$nextTick(() => {
+          new Swiper(this.$refs.swiper, {
+            direction: "horizontal", // 布局方向：horizontal / vertical
+            loop: true, // 循环
+            pagination: {
+              // 分页功能
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            navigation: {
+              // 导航箭头
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
